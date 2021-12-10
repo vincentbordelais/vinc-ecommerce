9ac92d1ec7ad515cb25d356016e2731bcef448c6
Vincent <vincent.bordelais.dev@gmail.com>
2021-12-10 15:32:03 +0100

    Page Panier, step 1

---
+++ src/Controller/CartController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart/add/{id}", name="cart_add", requirements={"id":"\d+"})
+     */
+    public function add($id, SessionInterface $session, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit {$id} n'existe pas !");
+        }
+
+        // 1 Retrouver le pannier dans la session (sous forme de tableau)
+        // 2 S'il n'existe pas encore je le crée en prenant un tableau vide
+        $cart = $session->get('cart', []);
+        // 3 Voir si le produit ($id) existe déjà dans le tableau
+        if (array_key_exists($id, $cart)) {
+            // 4 Si c'est le cas, simplement augmenter la quantité
+            $cart[$id]++;
+            // 5 Sinon ajouter le produit avec la quantité 1
+        } else {
+            $cart[$id] = 1;
+        }
+        // 6 Enregistrer le tableau mis à jour dans la session
+        $session->set('cart', $cart);
+
+        $this->addFlash('success', 'Le produit a bien été ajouté au pannier');
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug(),
+        ]);
+    }
+    /**
+     * @Route("/cart", name="cart_show")
+     */
+    public function show(SessionInterface $session, ProductRepository $productRepository)
+    {
+        $cart = $session->get('cart', []);
+        $detailedCart = [];
+        $total = 0;
+        foreach ($cart as $id => $qty) {
+            $product = $productRepository->find($id);
+            $detailedCart[] = [ // j'oublie toujours les []
+                'product' => $product,
+                'qty' => $qty,
+            ];
+            $total += $product->getPrice() * $qty; // le total général
+        }
+        // dd($detailedCart);
+        return $this->render('cart/index.html.twig', [
+            'items' => $detailedCart,
+            'total' => $total
+        ]);
+    }
+}
